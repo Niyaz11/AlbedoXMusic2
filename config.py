ccf4add5fbd6f59b191026be634414085eac589a
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-11-08 19:27:47 +0530

    Update config.py
    Signed-off-by: Niyaz11 <145466262+Niyaz11@users.noreply.github.com>

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Config:
         self.API_ID = "29245477"
         self.API_HASH = ("0abc83883262245c90ca337b7a0375c4")
 
-        self.BOT_TOKEN = ("8360416637:AAGjdc-snsZpaCKATMZeCm1hXBU22VH4_1s")
-        self.MONGO_URL = ("mongodb+srv://phrolovaxrobot:p0SFEz825QuqzwTi@cluster0.sy26fqm.mongodb.net/?retryWrites=true&w=majority")
+        self.BOT_TOKEN = ("")
+        self.MONGO_URL = ("")
 
         self.LOGGER_ID = (-1002456565415)
         self.OWNER_ID = (7654385403)
 
-        self.SESSION1 = "BQFnJzYAB4GU36CiFyCAZSMA6XV6Kt6UXNwFlK3mDROMWjehifYw-BpiKT-iK4benswKx_WysIeUDzvAclj6n9ytVeuQj1N318I2rlVy7YNqSTQfLvnuMeXqC--0cXcuBLkDgq-9E4uEcddedzS2CwU9LL7zL-NeV_kRGtaHeHsSZNbY8zD8EQ51sMsLKoqFdnGvgXyEVQ3Hra8ZXYIkZT4Cy0cSBkUIFbX0gp87SPMHD2UEZzgFJuPqbs33_7gxIcfmtCHRpOzR-7oJDNYHlDANqPa0xxWaNxXYI7oEMjC3xY-GDJjT8J9exPgGP5B8T4_wYWfbQNA5ZaGBg-cnJMld2HM5MwAAAAHY2x8OAA"
+        self.SESSION1 = ""
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
